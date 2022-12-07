c2ef50f938de6820544bbf04ccff9f429663f419
Watcharapon nuisin <38254231+godsnew2542@users.noreply.github.com>
2022-12-08 00:01:33 +0700

    add https

---
+++ assets/Appointment.dcb4179a.js
@@ -0,0 +1,1 @@
+import{at as t}from"./index.5f796ffe.js";const o=t("appointmentInfo",{state:()=>({id:null,obj:null})});export{o as A};
---
+++ assets/PatientAddOrEdit.01b12c1a.js
@@ -0,0 +1,1 @@
+import{at as t}from"./index.5f796ffe.js";const o=t("PatientProfile",{state:()=>({id:null,obj:{}})});export{o as P};
